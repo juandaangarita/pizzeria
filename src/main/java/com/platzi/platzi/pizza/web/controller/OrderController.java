0fab5dbd069a28c956c16335c278d67c03a856e9
Juan Angarita <juanan@backbase.com>
2024-03-22 14:08:43 -0500

    Adding cors configuration

--- src/main/java/com/platzi/platzi/pizza/web/controller/OrderController.java
+++ src/main/java/com/platzi/platzi/pizza/web/controller/OrderController.java
@@ -5,10 +5,7 @@ import com.platzi.platzi.pizza.persistence.projection.OrderSummary;
 import com.platzi.platzi.pizza.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
